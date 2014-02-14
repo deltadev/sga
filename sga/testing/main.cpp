87cd4c503b7f9671aa2ad8fc0a94b148483efe05
D.P. James <dj@deltadev.co.uk>
2014-02-14 13:33:31 +0000

    Added a testing project to get headers working. Added sparsehash source.

---
+++ sga/testing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testing
+//
+//  Created by Daniel James on 14/02/2014.
+//  Copyright (c) 2014 Daniel James. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
